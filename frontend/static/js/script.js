cb2dfd2a8700ccb1c1815544ffd9b202389517e7
Gonzalo Vignolles <gonzalosantacruzariel@gmail.com>
2025-02-19 22:59:56 -0300

    script

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -108,3 +108,17 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const img = entry.target;
+            img.src = img.dataset.src;
+            observer.unobserve(img);
+        }
+    });
+});
+
+document.querySelectorAll('img.lazy').forEach(img => {
+    observer.observe(img);
+});
